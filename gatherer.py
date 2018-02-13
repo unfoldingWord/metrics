b50271cf5c53e5acd0a99b5417ac4edea9f0cb69
Jesse Griffin <jag3773@gmail.com>
2018-02-13 13:01:25 -0500

    Updated hours for sprint 51

--- gatherer.py
+++ gatherer.py
@@ -12,11 +12,11 @@ logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 devs = [ ('bspidel', 2),
-         ('klappy', 5),
-         ('RoyalSix', 6),
+         ('klappy', 3),
+         ('RoyalSix', 5),
          ('mannycolon', 6),
          ('richmahn', 6),
-         ('PhotoNomad0', 6)
+         ('PhotoNomad0', 5)
        ]
 api_status = { 'complete': 0,
                'in-progress': 1,
