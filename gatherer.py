1ca98e6000ee592937496babcc8b2e6b2aa5dda9
Jesse Griffin <jag3773@gmail.com>
2018-07-10 18:38:58 -0400

    Updated for shorter sprint

--- gatherer.py
+++ gatherer.py
@@ -12,12 +12,12 @@ import datetime
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
-devs = [ ('bspidel', 3),
+devs = [ ('bspidel', 1),
          ('klappy', 0),
-         ('RoyalSix', 6),
-         ('mannycolon', 6),
-         ('richmahn', 6),
-         ('PhotoNomad0', 5)
+         ('RoyalSix', 3),
+         ('mannycolon', 3),
+         ('richmahn', 3),
+         ('PhotoNomad0', 3)
        ]
 api_status = { 'complete': 0,
                'in-progress': 1,
